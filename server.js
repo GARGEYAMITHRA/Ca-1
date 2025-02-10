04c8119fc7d5597b1aee03f755a277de55bcb153
GARGEYAMITHRA <gargeyamithra18gali@gmail.com>
2025-02-10 14:20:21 +0530

    done

---
+++ server.js
@@ -0,0 +1,28 @@
+const express=require(`express`)
+const cors=reuire(`cors`)
+const app=express()
+Port=5678
+
+app.use(express.json())
+app.use(cors)
+app.get('/',(req,res)=>{
+    res.status(200).send("Hello world!")
+})
+app.post('/signup',(req,res)=>{
+    const {username,email,passsword,dateofbirth}=app.req
+    if(!username){
+        return res.status(400).json({msg:"username cannot be empty"})
+    }
+    if(!email){
+        return res.status(400).json({msg:"Email cannot be empty"})
+    }
+    const c=passsword.length
+    if(c<8 || c>16){
+        return res.status(400).json({msg:"Password length should not be less than 8 or greater than 16"})
+    }
+
+})
+
+app.listen(Port,()=>{
+    console.log(`Succesfully connected ${Port}`)
+})
